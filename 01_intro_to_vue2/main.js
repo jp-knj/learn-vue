1e1fcb26ab2904c3faa857a2ce80d5e9ecf3b5b3
mono-tmt <tomita@monosus.co.jp>
2022-03-04 20:59:35 +0900

    :+1: class and style binding
    learn how to dynamically style our html by binding data on an element's
    style attribute

--- 01_intro_to_vue2/main.js
+++ 01_intro_to_vue2/main.js
@@ -2,13 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'this is a description',
         image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        onSale: false,
-        inventory: 100,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         variants: [
             {
                 variantId: 2234,
@@ -24,14 +20,11 @@ var app = new Vue({
         cart: 0
     },
     methods: {
-        addToCart(){
+        addToCart() {
             this.cart += 1
         },
         updateProduct(variantImage) {
             this.image = variantImage
-        },
-        removeFromCart() {
-            this.cart -= 1
         }
     }
-});
+})
