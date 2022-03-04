e2e0f3e1bcebef773679e79d65744fdb45292acf
mono-tmt <tomita@monosus.co.jp>
2022-03-04 20:22:15 +0900

    :tada: Initial Project

---
+++ 01_intro_to_vue2/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'this is a description',
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        onSale: false,
+        inventory: 100,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    }
+});
