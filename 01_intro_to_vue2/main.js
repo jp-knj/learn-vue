0b82afe352c5879f1f0e518b43d8782feb4ccef7
mono-tmt <tomita@monosus.co.jp>
2022-03-05 12:06:57 +0900

    :sparkles: computed properties
    calculate a value rather than store a value

--- 01_intro_to_vue2/main.js
+++ 01_intro_to_vue2/main.js
@@ -26,5 +26,22 @@ var app = new Vue({
         updateProduct(variantImage) {
             this.image = variantImage
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return  this.brand + ' ' + this.product + ' are not on sale'
+        }
     }
 })
